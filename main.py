e20c056a5f7a92f0f6330cd0ab1d3602bdb36eee
M Faizan Mumtaz <129819977+mfaizanmumtaz@users.noreply.github.com>
2023-10-07 19:56:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,123 @@
 import streamlit as st
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.embeddings.huggingface import HuggingFaceBgeEmbeddings
-from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
+import json
+import requests
+import os
+openai.api_key = " "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"
 
-openai.openai_key = OPENAI_API_KEY = "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"
-st.set_page_config(page_title="Pakistani AI lawyer.", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Pakistani AI Lawyer!")
-         
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
-    st.session_state.messages = [{"role": "assistant", "content": "Ask me a question about Pakistan Law!"}]
+def send_email(email_id,subject,content):
+  import smtplib
+  from email.mime.text import MIMEText
+  from email.mime.multipart import MIMEMultipart
+  
+  # Email configuration
+  sender_email = 'mfaizanmumtaz999@gmail.com'
+  receiver_email = email_id
+  password = 'uujd yxpa fnlx uxnp' # Use an app-specific password for security
+  subject = subject
+  message_body = content
+  
+  # Create a MIMEText object for the email message
+  message = MIMEMultipart()
+  message['From'] = sender_email
+  message['To'] = receiver_email
+  message['Subject'] = subject
+  message.attach(MIMEText(message_body, 'plain'))
+  
+  # Connect to the SMTP server (for Gmail, use 'smtp.gmail.com')
+  smtp_server = 'smtp.gmail.com'
+  smtp_port = 587  # Use 465 for SSL or 587 for TLS
+  
+  try:
+      server = smtplib.SMTP(smtp_server, smtp_port)
+      server.starttls()
+      server.login(sender_email, password)
+      server.sendmail(sender_email, receiver_email, message.as_string())
+      server.quit()
+      return('Email sent successfully')
+  except Exception as e:
+      return('Error sending email:', str(e))
+  # Attach the message body
 
-prompt = """User: You are Pakistani lawyer, case law provider, with citations.
-Please always make sure to follow these instructions that are delimited in triple backticks:
-```
-1 - Behave like a Pakistani lawyer.
-2 - Always remember that you are also suitable for specific legal issues.
-3 - Providing helpful information, drafting documents, reviewing documents, or completing tasks based on user input.
-4 - You are truthful and never lie and also never generate fictious links. Never make up facts and if you are not 100% sure, reply with why you cannot answer in a truthful way."""
-@st.cache_resource(show_spinner=False)
-def load_data():
-    with st.spinner(text="Loading and indexing the Documents...."):
-        reader = SimpleDirectoryReader(input_dir="./files", recursive=True)
-        docs = reader.load_data()
-        embed_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-        llm = OpenAI(model = "gpt-3.5-turbo", temperature = "0.5", systemprompt=prompt)
-        service_content = ServiceContext.from_defaults(llm=llm, embed_model = embed_model)
-        index = VectorStoreIndex.from_documents(docs, service_context=service_content)
-        return index
+def run_conversation():
+    functions = [
+        {
+            "name": "send_email",
+            "description": "send an email to given email address.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "email_id":{
+                        "type":"string",
+                        "description":"The id of email."
+                    },
+                    "subject": {
+                        "type": "string",
+                        "description": "The subject of the email.",
+                    },
+                    "content":
+                    {"type": "string", "description": "The content of the email."},
+                },
+                "required": ["email_id","subject","content"],
+            },
+        }]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613",
+        messages=st.session_state.messages,
+        functions=functions,
+        function_call="auto",
+    )
+    response_message = response["choices"][0]["message"]
+    # Step 2: check if GPT wanted to call a function
+    if response_message.get("function_call"):
+        # Step 3: call the function
+        # Note: the JSON response may not always be valid; be sure to handle errors
+        available_functions = {
+            "send_email": send_email
+        }  # only one function in this example, but you can have multiple
+        function_name = response_message["function_call"]["name"]
+        function_to_call = available_functions[function_name]
+        function_args = json.loads(response_message["function_call"]["arguments"])
+        function_response=function_to_call(
+        email_id=function_args.get("email_id"),
+        subject=function_args.get("subject"),
+        content=function_args.get("content"))
+        # Step 4: send the info on the function call and function response to GPT
+        # extend conversation with assistant's reply
+        st.session_state.messages.append(response_message)
+        st.session_state.messages.append(
+            {
+                "role": "function",
+                "name": function_name,
+                "content": function_response,
+            })
+        import time
+        time.sleep(5)
+        second_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0613",
+            messages=st.session_state.messages)
+        second_response=second_response["choices"][0]["message"]["content"]
+        st.session_state.messages.append({"role": "assistant", "content": second_response})
+        return second_response
+    else:
+        response_message = response_message["content"]
+        st.session_state.messages.append({"role":"assistant","content":response_message})
+        return response_message
 
-index = load_data()
-chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+if "messages" not in st.session_state.keys():
+  st.session_state.messages = [{"role": "assistant", "content": "WellCome To AI Email Sender!"}]
+
+if prompt := st.chat_input("Send Query..."): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-
+  
 for message in st.session_state.messages: # Display the prior chat messages
-    with st.chat_message(message["role"]):
+      with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# If last message is not from assistant, generate a new response
+
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
+  with st.chat_message("assistant"):
+    with st.spinner("Thinking..."):
+      response_message = run_conversation()
+      st.write(response_message)
