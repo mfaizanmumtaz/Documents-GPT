e5e33cda14fdd160b797cb2627bcc762572d3568
M Faizan Mumtaz <129819977+mfaizanmumtaz@users.noreply.github.com>
2023-10-07 12:29:04 +0000

    new

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import openai
 from llama_index import SimpleDirectoryReader
 
 
-openai.api_key = "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"
+# openai.api_key = "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"
+
+openai.openai_key = st.secrets["OPENAI_API_KEY"]
 
 st.set_page_config(page_title="Pakistani AI lawyer.", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("Pakistani AI Lawyer!")
